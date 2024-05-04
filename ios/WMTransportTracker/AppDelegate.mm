53539d8816e8531459a782d442d2a9741d53c4cf
Carl Ansell <10620927+carlansell94@users.noreply.github.com>
2024-05-04 11:34:41 +0100

    build: Update for React Native v0.74

--- ios/WMTransportTracker/AppDelegate.mm
+++ ios/WMTransportTracker/AppDelegate.mm
@@ -16,11 +16,13 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
+  return [self bundleURL];
+}
+ 
+- (NSURL *)bundleURL
+{
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
-#else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
 }
 
 @end
